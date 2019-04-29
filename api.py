1e568e90a92abb97baf985f01166dc963ef40d73
angelicavi7 <angelicaviviana7@gmail.com>
2019-04-28 21:21:42 -0500

    Add files via upload

---
+++ api.py
@@ -0,0 +1,40 @@
+from flask import Flask
+from flask_restplus import Api, Resource, fields
+from sklearn.externals import joblib
+from m09_model_deployment1 import predict_gender
+
+app = Flask(__name__)
+
+api = Api(
+    app, 
+    version='1.0', 
+    title='Movie Genre Prediction API',
+    description='Movie Genre Prediction API')
+
+ns = api.namespace('Predict', 
+     description='Predict Genre')
+   
+parser = api.parser()
+
+parser.add_argument('Plot',type=str,required=True,help='Movie Plot',location='args')
+parser.add_argument('Title',type=str,required=True,help='Movie Title',location='args')
+parser.add_argument('Year',type=int,required=True,help='Movie Released Year',location='args')
+
+resource_fields = api.model('Resource', {
+    'result': fields.String,
+})
+
+@ns.route('/')
+class MovieGenderApi(Resource):
+
+    @api.doc(parser=parser)
+    @api.marshal_with(resource_fields)
+    def get(self):
+        args = parser.parse_args()
+        
+        return {
+         "result": predict_gender(args["Plot"],args["Title"],args["Year"])
+        }, 200
+
+if __name__ == '__main__':
+    app.run(use_reloader=False, host='0.0.0.0', port=8888)
